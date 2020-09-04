4aae881d1f406ad8506ca0695b0c7eb5e609b6f4
Tanmay <68606780+TnmyB@users.noreply.github.com>
2020-09-04 19:38:55 +0530

    Update anagram.cpp

--- anagram.cpp
+++ anagram.cpp
@@ -1,13 +1,58 @@
 #include "anagram.h"
 
+void removeSpace(std::string& w) {
+    int count = 0;
+    for (int i = 0; w[i]; i++) {
+        if (w[i] != ' ') {
+            w[count] = w[i];
+            count++;
+        }
+    }
+    w[count] = '\0';
+}
+
+
+void string_align(std::string& w) {
+    transform(w.begin(), w.end(), w.begin(), ::tolower);
+    
+    //removeSpace(w);
+    
+    w.erase(remove(w.begin(), w.end(), ' '),w.end());
+}
+
+
 bool Anagram::WordPairIsAnagram(const std::string& word1, const std::string& word2) {
     //Fill the correct implementation here
-    return false;
+    
+    string w1 = word1;
+    string w2 = word2;
+    
+    string_align(w1);
+    string_align(w2);
+    
+    if (w1.size() != w2.size())
+        return false;
+
+    sort(w1.begin(), w1.end());
+    sort(w2.begin(), w2.end());
+    
+    int length = w1.size();
+    for (int i = 0; i < length; i++) {
+        if (w1[i] != w2[i])
+            return false;
+    }
+    return  true;
 }
 
 std::vector<std::string> Anagram::SelectAnagrams(
         const std::string& word,
         const std::vector<std::string>& candidates) {
     //Fill the correct implementation here
+    for (unsigned int i = 0; i < candidates.size();i++) {
+        if (!Anagram::WordPairIsAnagram(word, candidates[i])) {
+            candidates.erase(candidates.begin()+i);
+            i--;
+        }
+    }
     return candidates;
 }
